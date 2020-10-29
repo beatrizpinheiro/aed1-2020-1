adcdf38036104171a16dfc9fc2de0c193cc11514
IngridPs <71459442+IngridPs@users.noreply.github.com>
2020-10-29 14:01:08 -0300

    Update trabalhoAED1.c

--- trabalhoAED1.c
+++ trabalhoAED1.c
@@ -36,12 +36,80 @@ int faltas(int carga){ // calcula quantas faltas o aluno pode ter
 
 }
 
+void provas(){
 
+	int nt,nf,i;
+	int *pesos;
+	float *notas;
+	float mnf=0,mf=0,np;
+	int pnf =0,ps=0,pt=0;
 
-int main(){
+	printf("Número total de provas: ");
+	scanf("%d" ,&nt);
+	
+	pesos = malloc(nt * sizeof(int));
+	
+	for(i=0;i<nt;i++){
+		
+		printf("Peso prova %d: ",(i+1));
+		scanf("%d" ,&pesos[i]);
+	}
+	
+	printf("Número de provas já feitas: ");
+	scanf("%d" ,&nf);
+	
+	notas = malloc(nf * sizeof(float));
+	
+	for(i=0;i<nf;i++){
+		
+		printf("Nota prova %d: " ,(i+1));
+		scanf("%f" ,&notas[i]);
+	}
+	
+	for(i=0;i<nt;i++){
+		pt = pt + pesos[i];	
+	}
+	
+	if(nf == nt){
+		for(i=0;i<nt;i++){
+			mf = mf + notas[i]*pesos[i];
+		}
+		
+		mf = mf/pt;
+	
+		printf("Sua média final será de %.2f\n" ,mf);
+	}
+	
+	else{
+	
+		for(i=0;i<nf;i++){
+	
+			mnf = mnf + notas[i]*pesos[i];
+			pnf = pnf + pesos[i];		
+	
+		}
+	
+	
+		for(i=nf;i<nt;i++){
+			ps = ps + pesos[i];	
+		}
+	
+	
+		np = ((6*pt) - mnf)/ps;
+	
+		printf("É necessário tirar no minímo %.2f na(s) %d prova(s) restante(s) para passar \n" ,np,(nt-nf));
+	
+	
+	}
 
-	ALUNO x;
-	int i,k,d;
+	
+	free(notas);
+	free(pesos);
+
+}
+
+ALUNO cadastro(ALUNO *x, AULA *y){
+    int i,k,d;
 	FILE *pont_arq;
 
 	pont_arq = fopen("EAD.txt", "a");
@@ -77,45 +145,51 @@ int main(){
 
 	for(i=0;i<(x.quantMaterias);i++){
 
-			printf("Nome da matéria %d: " ,(i+1));
-			scanf("%s" ,y[i].nomeAula);
-			getchar();
+		printf("Nome da matéria %d: " ,(i+1));
+		scanf("%s" ,y[i].nomeAula);
+		getchar();
 			
-			printf("Carga horária: ");
-			scanf("%d" ,&y[i].cargaH);
+		printf("Carga horária: ");
+		scanf("%d" ,&y[i].cargaH);
 			
-			y[i].quantAulaSemana = y[i].cargaH/16; //quantas aulas tem essa materia por semana
+		y[i].quantAulaSemana = y[i].cargaH/16; //quantas aulas tem essa materia por semana
 
-			printf("Quantos dias da semana você tem essa aula?: ");
-			scanf("%d" ,&y[i].quantDiasSemana);
+		printf("Quantos dias da semana você tem essa aula?: ");
+		scanf("%d" ,&y[i].quantDiasSemana);
 
-			y[i].numAulasDia = y[i].quantAulaSemana/y[i].quantDiasSemana;
+		y[i].numAulasDia = y[i].quantAulaSemana/y[i].quantDiasSemana;
 			
-			y->diasSemana = (char*) malloc(y[i].quantDiasSemana * sizeof(char*));
-			y[i].diasSemana = (int*) malloc(10 * sizeof(int)); 			
+		y[i]->diasSemana = (char**) malloc(y[i].quantDiasSemana * 10 * sizeof(char*));
 			
-			for(k=0;k<y[i].quantAulaSemana;k++){
+		for(k=0;k<y[i].quantAulaSemana;k++){
 				
-				for(d=0;d<y[i].quantDiasSemana;d++){
-					printf("Em quais dias da semana você tem essa aula? Separe com espaços.");
-					scanf("%s" ,&y[d].diasSemana);
-				}
-				getchar();
-
-				printf("Turno %d: ",k+1);
-				scanf("%s",&y[k].turno);
-				printf("\n");
-
- 				getchar();
-
-				printf("Horario da aula %d: ",k+1);
-				scanf("%d",&y[k].horario);
-				printf("\n");
+			for(d=0;d<y[i].quantDiasSemana;d++){
+				y[i][d].diasSemana = (char**) malloc(10 * sizeof(char*));
+				printf("Em quais dias da semana você tem essa aula? Separe com espaços.");
+				scanf("%s" ,&y[i][d].diasSemana); //exemplo: segunda e quarta
 			}
-			
-			free(y[i].diasSemana);
-			free(y);
+			getchar();
+
+			printf("Turno %d: ",k+1);
+			scanf("%s",&y[k].turno);
+			printf("\n");
+
+ 			getchar();
+
+			printf("Horario da aula %d: ",k+1);
+			scanf("%d",&y[k].horario);
+			printf("\n");
 		}
+			
+		free(y[i]->diasSemana);
+		free(y);
+	}
+}
+
+int main(){
+    
+	
+	
 	
 	
 			
